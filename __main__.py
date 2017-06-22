fbf95253c104ecc1f9c6459b3b36be7ac1fa848d
Ocalik <vincent.segouin@gmail.com>
2017-06-22 19:43:17 +0200

    test camera

--- __main__.py
+++ __main__.py
@@ -1,8 +1,6 @@
 from utils.ImageUtils import ImageUtils
 from utils.Rekognition import Rekognition
+from utils.CameraUtils import PiCamUtils
 
-image = ImageUtils.encode_img("assets/selfie.jpg")
-aw = Rekognition()
-
-facesResponse = aw.detect_face(raw_img=image)
-facesResponse = aw.detect_labels(image_raw=image)
+cam = PiCamUtils()
+cam.start_preview()
