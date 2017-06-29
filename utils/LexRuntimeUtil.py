53063fab26480a59d7295551d2e1651ca3f8c624
vsegouin <vsegouin@xebia.fr>
2017-06-29 22:58:29 +0200

    Added Lex implementation Added a Recording util

--- utils/LexRuntimeUtil.py
+++ utils/LexRuntimeUtil.py
@@ -5,14 +5,37 @@ class LexRuntimeUtil:
     def __init__(self):
         self.lex_client = boto3.client('lex-runtime')
 
-    def say_something(self):
-        response = self.lex_client.post_text(
-            botName='TodaysWeather',
-            botAlias='inspiration',
-            userId='aaa',
-            sessionAttributes={'string': 'string'},
-            inputText='Say something interresting',
-        )
-        return response['message']
+    def start_discussion(self):
+        dialogState = ''
+        message = 'What do you want ?'
+        while dialogState == '' or dialogState == 'ElicitSlot' or dialogState == 'ElicitIntent':
+            inputText = raw_input(message)
+            response = self.lex_client.post_text(
+                botName='WeatherInWorld',
+                botAlias='TodayWeather',
+                userId='aaa',
+                sessionAttributes={},
+                inputText=inputText,
+            )
+            dialogState = response['dialogState']
+            message = response['message']
+        return message
 
+    def start_speech(self):
+        dialogState = ''
+        message = 'What do you want ?'
+        while dialogState == '' or dialogState == 'ElicitSlot' or dialogState == 'ElicitIntent':
+            inputText = raw_input(message)
+            response = self.lex_client.post_content(
+                botName='WeatherInWorld',
+                botAlias='TodayWeather',
+                userId='aaa',
+                sessionAttributes={},
+                contentType='text/plain; charset=utf-8',
+                accept='text/plain; charset=utf-8',
+                inputStream=inputText
+            )
+            dialogState = response['dialogState']
+            message = response['message']
+        return message
 
