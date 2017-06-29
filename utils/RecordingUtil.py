45b4b0a5226d3f07781c2b8c54c729cd2e473eab
Ocalik <vincent.segouin@gmail.com>
2017-06-30 01:39:11 +0200

    Updated the recording utils

--- utils/RecordingUtil.py
+++ utils/RecordingUtil.py
@@ -1,4 +1,8 @@
+import wave
+import random
+import struct
 import numpy
+import math
 import pyaudio
 import analyse
 
@@ -8,7 +12,7 @@ pyaud = pyaudio.PyAudio()
 device_index = 2
 CHANNELS = 1
 AUDIO_SAMPLING_RATE = 48000
-AUDIO_BUFFER_SIZE = 1024
+AUDIO_BUFFER_SIZE = 8192
 FORMAT=pyaudio.paInt16
 
 # Open input stream, 16-bit mono at 48000 Hz
@@ -22,6 +26,23 @@ stream = pyaud.open(
     frames_per_buffer=AUDIO_BUFFER_SIZE,
     input_device_index=device_index
 )
+sampleRate = 44100.0 # hertz
+duration = 1.0       # seconds
+frequency = 440.0    # hertz
+
+wavef = wave.open('sound.wav','w')
+wavef.setnchannels(1) # mono
+wavef.setsampwidth(2)
+wavef.setframerate(sampleRate)
+
+for i in range(int(duration * sampleRate)):
+    value = int(32767.0*math.cos(frequency*math.pi*float(i)/float(sampleRate)))
+    data = struct.pack('<h', value)
+    wavef.writeframesraw( data )
+
+wavef.writeframes('')
+wavef.close()
+
 while True:
     # Read raw microphone data
     rawsamps = stream.read(1024)
